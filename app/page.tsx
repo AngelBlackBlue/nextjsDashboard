9d9147f303f6a191a08dfc7ce67396adb22aed54
AngelBlackBlue <angel.blackbkue@hotmail.com>
2023-11-23 22:20:46 -0300

    error en invoices/table

--- app/page.tsx
+++ app/page.tsx
@@ -36,14 +36,14 @@ const Page = () => {
              width={1000}
              height={760}
              className="hidden md:block"
-             alt="desktop"
+             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
              src="/hero-mobile.png"
              width={560}
              height={620}
              className="block md:hidden"
-             alt="mobile"
+             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
       </div>

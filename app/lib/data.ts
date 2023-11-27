80904d2b1d9a513fa1e5f1a26f5e6de730dca13a
AngelBlackBlue <angel.blackbkue@hotmail.com>
2023-11-27 08:23:05 -0300

    capitulo 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,10 +9,12 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache'
 
-export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+export const fetchRevenue = async () => {
+  // Add noStore() here prevent the response from being cached. // Agregue noStore() aquí para evitar que la respuesta se almacene en caché.
+  // This is equivalent to in fetch(..., {cache: 'no-store'}). // Esto es equivalente a in fetch(..., {cache: 'no-store'}).
+  noStore()
 
   try {
     // Artificially delay a response for demo purposes.
@@ -33,6 +35,7 @@ export async function fetchRevenue() {
 }
 
 export const fetchLatestInvoices = async () =>{
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +56,7 @@ export const fetchLatestInvoices = async () =>{
 }
 
 export const fetchCardData = async() => {
+  noStore()
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -88,10 +92,11 @@ export const fetchCardData = async() => {
 }
 
 const ITEMS_PER_PAGE = 6;
-export async function fetchFilteredInvoices(
+export const fetchFilteredInvoices = async (
   query: string,
   currentPage: number,
-) {
+) => {
+  noStore()
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -123,7 +128,8 @@ export async function fetchFilteredInvoices(
   }
 }
 
-export async function fetchInvoicesPages(query: string) {
+export const fetchInvoicesPages = async (query: string) => {
+  noStore()
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -144,7 +150,8 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
-export async function fetchInvoiceById(id: string) {
+export const fetchInvoiceById = async (id: string) => {
+  noStore()
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -169,7 +176,8 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
-export async function fetchCustomers() {
+export const fetchCustomers = async () => {
+  noStore()
   try {
     const data = await sql<CustomerField>`
       SELECT
